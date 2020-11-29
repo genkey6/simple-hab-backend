27f022f39cd14279e428bc136caa94e40598e8ba
Shu Gen <gen_shu@caddi.jp>
2020-11-30 01:32:16 +0900

    add diesel

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,16 @@
+#[macro_use]
+extern crate diesel;
+extern crate dotenv;
+
 pub mod api;
-mod error;
-mod models;
+pub mod error;
+pub mod models;
+pub mod schema;
 pub mod views;
 
+use diesel::pg::PgConnection;
+use diesel::prelude::*;
+use dotenv::dotenv;
 pub use error::MyError;
 pub use models::User;
+use std::env;
