f76b29929c2750dcf373c948e6436b5637bd585f
Shu Gen <gen_shu@caddi.jp>
2020-12-11 12:54:44 +0900

    update models.rs

--- src/models.rs
+++ src/models.rs
@@ -1,7 +1,56 @@
+use crate::db;
+use crate::error::MyError;
+use crate::schema::users;
+
+use diesel::prelude::*;
 use serde::{Deserialize, Serialize};
 
-#[derive(Clone, Debug, Serialize, Deserialize)]
+#[derive(Clone, Debug, Serialize, Deserialize, Queryable, Insertable)]
+#[table_name = "users"]
 pub struct User {
-  id: u32,
+  id: i32,
   name: String,
+}
+
+impl User {
+  pub fn find() -> Result<Vec<Self>, MyError> {
+    let conn = db::connection()?;
+    let users = users::table.load::<User>(&conn)?;
+    Ok(users)
+  }
+
+  pub fn get(id: i32) -> Result<Self, MyError> {
+    let conn = db::connection()?;
+    let user = users::table.filter(users::id.eq(id)).first(&conn)?;
+    Ok(user)
+  }
+
+  pub fn create(command: UserCommand) -> Result<Self, MyError> {
+    let conn = db::connection()?;
+    let user = diesel::insert_into(users::table)
+        .values(command)
+        .get_result(&conn)?;
+    Ok(user)
+  }
+
+  pub fn update(id: i32, command:UserCommand) -> Result<Self, MyError> {
+    let conn = db::connection()?;
+    let user = diesel::update(users::table)
+        .filter(users::id.eq(id))
+        .set(command)
+        .get_result(&conn)?;
+    Ok(user)
+  }
+
+  pub fn delete(id: i32) -> Result<usize, MyError> {
+    let conn = db::connection()?;
+    let res = diesel::delete(users::table.filter(users::id.eq(id))).execute(&conn)?;
+    Ok(res)
+  }
+}
+
+#[derive(Serialize, Deserialize, AsChangeset, Insertable)]
+#[table_name = "users"]
+pub struct UserCommand {
+  pub name: String,
 }
