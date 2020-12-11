314811b872286032cbd5fcf5235b5367b1fc4af1
Shu Gen <gen_shu@caddi.jp>
2020-12-11 12:56:04 +0900

    update modules

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,15 @@
 #[macro_use]
 extern crate diesel;
+#[macro_use]
+extern crate diesel_migrations;
 extern crate dotenv;
 
 pub mod api;
+pub mod db;
 pub mod error;
 pub mod models;
 pub mod schema;
 pub mod views;
 
-use diesel::pg::PgConnection;
-use diesel::prelude::*;
-use dotenv::dotenv;
 pub use error::MyError;
 pub use models::User;
-use std::env;
