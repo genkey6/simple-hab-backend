ea609c2024fcfb2da545f70a92e690f621996a5a
Shu Gen <gen_shu@caddi.jp>
2020-12-11 12:55:24 +0900

    update error.rs

--- src/error.rs
+++ src/error.rs
@@ -1,19 +1,53 @@
-use actix_web::ResponseError;
-use thiserror::Error;
+use actix_web::http::StatusCode;
+use actix_web::{HttpResponse, ResponseError};
+use diesel::result::Error as DieselError;
+use serde::Deserialize;
+use serde_json::json;
+use std::fmt;
 
-// Error をまとめる enum を定義する
-// actix_web::ResponseError として使うために derive マクロで Debug を付与する必要がある
-#[derive(Error, Debug)]
-pub enum MyError {
-  #[error("Failed to render HTML")]
-  AskamaError(#[from] askama::Error),
-
-  #[error("Failed to get connection")]
-  ConnectionPoolError(#[from] r2d2::Error),
-
-  #[error("Failed SQL execution")]
-  SQLiteError(#[from] rusqlite::Error),
+#[derive(Debug, Deserialize)]
+pub struct MyError {
+  pub error_status_code: u16,
+  pub error_message: String,
 }
 
-// actix_web::ResponseError を MyError に実装
-impl ResponseError for MyError {}
+impl MyError {
+  pub fn new(error_status_code: u16, error_message: String) -> MyError {
+    MyError {
+      error_status_code,
+      error_message,
+    }
+  }
+}
+
+impl fmt::Display for MyError {
+  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+    f.write_str(self.error_message.as_str())
+  }
+}
+
+impl From<DieselError> for MyError {
+  fn from(error: DieselError) -> MyError {
+    match error {
+      DieselError::DatabaseError(_, err) => MyError::new(409, err.message().to_string()),
+      DieselError::NotFound => MyError::new(404, "record not found".to_string()),
+      err => MyError::new(500, format!("Unknown Diesel error: {}", err)),
+    }
+  }
+}
+
+impl ResponseError for MyError {
+  fn error_response(&self) -> HttpResponse {
+    let status_code = match StatusCode::from_u16(self.error_status_code) {
+      Ok(status_code) => status_code,
+      Err(_) => StatusCode::INTERNAL_SERVER_ERROR,
+    };
+
+    let error_message = match status_code.as_u16() < 500 {
+      true => self.error_message.clone(),
+      false => "Internal server error".to_string(),
+    };
+
+    HttpResponse::build(status_code).json(json!({ "message": error_message }))
+  }
+}
