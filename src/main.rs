4015358bec9ddb02eb4b7a0f9bbbcb95d275d2bd
Shu Gen <gen_shu@caddi.jp>
2020-12-11 12:56:26 +0900

    update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-use actix_web::{get, web, App, HttpResponse, HttpServer};
-use r2d2::Pool;
-use r2d2_sqlite::SqliteConnectionManager;
-use rusqlite::params;
-use simple_hab_backend::api::{delete_user, get_user, list, post_user};
+use actix_web::{get, App, HttpResponse, HttpServer};
+use dotenv::dotenv;
+
+use simple_hab_backend::api::init_routes;
+use simple_hab_backend::db;
 use simple_hab_backend::MyError;
 
 #[get("/hello")]
@@ -17,32 +17,15 @@ async fn hello() -> Result<HttpResponse, MyError> {
 
 #[actix_rt::main]
 async fn main() -> Result<(), actix_web::Error> {
-    let manager = SqliteConnectionManager::file("user.db");
-    let pool = Pool::new(manager).expect("Failed to initialize the connection pool.");
-    let conn = pool
-        .get()
-        .expect("Failed to get the connection from the pool.");
-    conn.execute(
-        "CREATE TABLE IF NOT EXISTS user (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            name TEXT NOT NULL
-        )",
-        params![],
-    )
-    .expect("Failed to create a table 'user'.");
+    dotenv().ok();
+
+    std::env::set_var("RUST_LOG", "actix_web=debug");
+    env_logger::init();
+    db::init();
 
     // HttpServer::new に App を作る関数を渡してサーバーを起動できる
     // App には service() という関数があり、App::new().service(hoge) のように関数を渡して登録する
-    let app = move || {
-        App::new().service(hello).service(
-            web::scope("/")
-                .data(pool.clone())
-                .service(list)
-                .service(get_user)
-                .service(post_user)
-                .service(delete_user),
-        )
-    };
+    let app = move || App::new().service(hello).configure(init_routes);
     HttpServer::new(app).bind("0.0.0.0:8080")?.run().await?;
     Ok(())
 }
