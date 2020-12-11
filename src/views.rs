27e58cf8290b77af8b7f3b3df7ee8248cd7f4778
Shu Gen <gen_shu@caddi.jp>
2020-12-11 12:56:44 +0900

    comment out views.rs

--- src/views.rs
+++ src/views.rs
@@ -1,12 +1,12 @@
-use askama::Template;
+// use askama::Template;
 
-pub struct UserEntry {
-  pub id: u32,
-  pub name: String,
-}
+// pub struct UserEntry {
+//   pub id: u32,
+//   pub name: String,
+// }
 
-#[derive(Template)]
-#[template(path = "users.html")]
-pub struct UsersTemplate {
-  pub entries: Vec<UserEntry>,
-}
+// #[derive(Template)]
+// #[template(path = "users.html")]
+// pub struct UsersTemplate {
+//   pub entries: Vec<UserEntry>,
+// }
